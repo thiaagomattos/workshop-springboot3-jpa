a53a46b6a75b4d90471a884968c7861631135b0d
Thiago Mattos <euthiagomattos@gmail.com>
2024-09-26 15:16:39 -0300

    project created

---
+++ src/main/java/com/educandoweb/coursejava/CoursejavaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursejavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/coursejava/CoursejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
