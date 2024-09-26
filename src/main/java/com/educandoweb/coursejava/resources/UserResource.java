433c1c230f1d3c537716da4d5885b40d93cbea68
Thiago Mattos <euthiagomattos@gmail.com>
2024-09-26 16:06:12 -0300

    h2 database, test profile, jpa

--- src/main/java/com/educandoweb/coursejava/resources/UserResource.java
+++ src/main/java/com/educandoweb/coursejava/resources/UserResource.java
@@ -1,7 +1,5 @@
 package com.educandoweb.coursejava.resources;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
