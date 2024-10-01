fa2d1065d2d6f9f15b71df71d4ab05ee73220510
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-01 17:39:47 -0300

    user delete

--- src/main/java/com/educandoweb/coursejava/services/UserService.java
+++ src/main/java/com/educandoweb/coursejava/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.coursejava.entities.User;
@@ -27,4 +29,8 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+}
 }
