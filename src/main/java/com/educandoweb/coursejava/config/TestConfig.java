0056483eb70f95f92cad41bc9e3c76a225a85400
Thiago Mattos <euthiagomattos@gmail.com>
2024-10-11 11:14:20 -0300

    changed time in testCOnfig
    git push -u origin main

--- src/main/java/com/educandoweb/coursejava/config/TestConfig.java
+++ src/main/java/com/educandoweb/coursejava/config/TestConfig.java
@@ -82,7 +82,7 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
 		
-		Payment pay1 = new Payment(null, Instant.parse("2019-07-20T21:53:07Z"), o1);
+		Payment pay1 = new Payment(null, Instant.parse("2019-10-20T21:53:07Z"), o1);
 		o1.setPayment(pay1);
 		
 		orderRepository.save(o1);
